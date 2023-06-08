0451bdc121e89e276213dbff336d96b4050ceb67
developer006 <developer.ludovic@gmail.com>
2023-06-08 03:32:23 +0300

    add title to login/regiaster

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta charset="utf-8">
-		<title>Responsive Login Page</title>
+		<title>Tour Login Page</title>
 		<link rel="stylesheet" href="{{asset('auth/style.css')}}">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" charset="utf-8"></script>
